3ce6b1a0a4e40ee1a3e7c2206b8a5aaa84af3023
Ryan Cosyns <ryancosyns@comcast.net>
2020-03-03 09:29:28 -0700

    Add project files.

---
+++ Bayshore.CodeTest/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bayshore.CodeTest.Models;
+
+namespace Bayshore.CodeTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IndexViewModel vm = new IndexViewModel()
+            {
+                Number = 0,
+                TestComplete = false
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult CheckForPalindrom(IndexViewModel vm)
+        {
+            if(ModelState.IsValid)
+            {
+                vm.IsPalindrome = IsPalindrome(vm.Number.Value);
+                vm.TestComplete = true;
+                return View("Index", vm);
+            }
+
+            return View("Index", vm);
+        }
+
+        public bool IsPalindrome(int number)
+        {
+            IEnumerable<char> forwards = number.ToString().ToCharArray();
+            return forwards.SequenceEqual(forwards.Reverse());            
+        }
+    }
+}
---
+++ Bayshore.CodeTest/Models/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bayshore.CodeTest.Models
+{
+    public class IndexViewModel
+    {
+        [Required(ErrorMessage = "Number is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter a valid positive number")]
+        public int? Number { get; set; }
+        public bool IsPalindrome { get; set; }
+        public bool TestComplete { get; set; }
+    }
+}
